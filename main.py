65d92b7646672c1a9867ca0716eb785a1e990a15
aneshamdaoui <bacetd2004@gmail.com>
2023-10-27 17:41:03 +0100

    play around with python syntaxs

---
+++ main.py
@@ -0,0 +1,17 @@
+import constant
+
+# num1 = True ; 
+# num2 = "string" 
+# num3 = 145 ;
+
+num1 , num2 , num3 = True , "string" ,145  
+print('old values of num 1' ,num1,'num3 : ' , num3)
+num3 = num1 = 45 
+# make a return from this
+
+print('hello and welcom to this letter \n', sep='', end='')
+
+
+# use same constant 
+
+print('Pi :',constant.PI,'Gravity :',constant.GRAVITY) 
